83723930c2ec88365429708f4ff0ee5292b8e031
Kirill <KErmakov@lanit.ru>
2019-01-22 20:20:50 +0300

    Добавлен класс Point и использована функция вычесления расстояния между двумя точками

--- sendbox/src/main/java/FirstProgram.java
+++ sendbox/src/main/java/FirstProgram.java
@@ -8,5 +8,9 @@ public class FirstProgram {
 
         Square s = new Square(8);
         System.out.println("Площадь квадрата со стороной " + s.a + " = " + s.area());
+
+        Point t1 = new Point (2,5);
+        Point t2 = new Point (4,8);
+        System.out.println("Расстояние между двумя точками на плоскости " + t1.x + ";" + t1.y + " и " + t2.x + ";" + t2.y + " = " + t1.distance(t2));
     }
 }
